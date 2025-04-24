417c591100b2f7023e5aa80e77dbc34c26d0d013
Lorran2025 <lorranpereiraslv@gmail.com>
2025-04-24 01:15:19 +0000

    Arquivo Atualizado

--- xadrez.c
+++ xadrez.c
@@ -1,40 +1,57 @@
 #include <stdio.h>
 
-int main(){
-
-    int i;
-    int movimentoCompleto = 1;
-
-    //Movimentação da torre
-    i = 1;
-    while (i <= 5)
-    {
+//Movimentação da Torre
+void moverTorre(int casas){
+    if(casas > 0) {
         printf("Torre: Direita \n");
-        i++;
+        moverTorre(casas - 1);
     }
-    //Movimentação do bispo
-    i = 1;
-    do
-    {
-        printf("Bispo: Cima, direita \n");
-        i++;
+}
 
-    } while (i <= 5);
-    //Movimentação da rainha
-    for (i = 1; i <= 8; i++)
-    {
+//Movimentação do Bispo (Com Recursividade)
+void moverBispo(int casas){
+    if (casas > 0){   
+        printf("Bispo: Cima, Direita \n");
+        moverBispo(casas - 1);
+    }
+}
+
+//Movimentação do Bispo (Com Loops Aninhados)
+void moverBispoComLoopsAninhados(int casas){
+    for (int i = 0; i < casas; i++){
+    for (int j = 0; j < 1; j++){
+        printf("Bispo (Loop Aninhado): Cima, Direita \n");
+}
+}
+}
+
+//Movimentação da Rainha
+void moverRainha(int casas){
+    if(casas > 0){
         printf("Rainha: Esquerda \n");
+        moverRainha(casas - 1);
+    }
+}
 
+//Movimentação do Cavalo
+void moverCavalo(int casas){
+    for (int i = 0; i < casas; i++){
+    for (int j = 0; j < casas; j++){
+        if (i == j){
+            printf("Cavalo: Cima\n");
+            printf("Cavalo: Cima\n");
+            printf("Cavalo: Direita\n");
+        }
     }
-    
-    //Movimentação do cavalo
-    while (movimentoCompleto--)
-    {
-        for (int i = 1; i <= 2; i++){
-            printf("Cavalo: Cima \n");
-        } 
-            printf("Cavalo: Direita \n");
     }
-    
+    }
+
+int main(){
+    moverTorre(5);
+    moverBispo(5);
+    moverBispoComLoopsAninhados(5);
+    moverRainha(8);
+    moverCavalo(1);
     return 0;
 }
+
